3c7ee67ff00cf38eadfef385ee0552df81045049
lukablaskovic <lukablaskovic2000@gmail.com>
2022-12-01 19:47:02 +0100

    added some colors and icons

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -2,20 +2,42 @@
   <v-container>
     <v-row class="text-center">
       <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
+        <h1 class="display-2 font-weight-bold mb-3 text-gray-700">
           Student enquiries classificator
         </h1>
-        <v-textarea
-          v-model="inquiry"
-          name="input-7-1"
-          label="Unesite vaš upit ovdje"
-          hint="Pitanje postavite na Hrvatskom jeziku"
-        ></v-textarea>
 
-        <v-btn class="mx-2" fab dark small color="primary" @click="sendInquiry">
-          <v-icon dark> mdi-email-arrow-right </v-icon>
-        </v-btn>
-        <v-container class="w-1/2 mt-6">
+        <v-container class="max-w-md">
+          <v-textarea
+            v-model="inquiry"
+            name="input-7-1"
+            label="Unesite vaš upit ovdje"
+            hint="Pitanje postavite na Hrvatskom jeziku"
+            append-icon="mdi-message-question"
+            auto-grow>
+          </v-textarea>
+        </v-container>
+        <v-tooltip bottom v-if="!request">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              class="mx-2"
+              fab
+              dark
+              small
+              color="primary"
+              @click="sendInquiry"
+              v-bind="attrs"
+              v-on="on">
+              <v-icon dark> mdi-email-arrow-right </v-icon>
+            </v-btn>
+          </template>
+          <span>Pošalji upit</span>
+        </v-tooltip>
+        <v-progress-circular
+          v-if="request"
+          indeterminate
+          color="primary"></v-progress-circular>
+
+        <v-container class="w-1/2 mt-6 max-w-md">
           <v-simple-table dense fixed-header>
             <template v-slot:default>
               <thead>
@@ -27,13 +49,29 @@
               <tbody>
                 <tr v-for="topic in topics" :key="topic.name">
                   <td>{{ topic.topic }}</td>
-                  <td>{{ topic.similarity }}</td>
+                  <td>
+                    <v-chip :color="topic.color">
+                      {{ topic.similarity }}
+                    </v-chip>
+                  </td>
                 </tr>
               </tbody>
             </template>
           </v-simple-table>
         </v-container>
       </v-col>
+    </v-row>
+
+    <v-row>
+      <v-footer class="mb-1" color="white" padless bottom absolute>
+        <v-row justify="center" no-gutters>
+          <v-img
+            max-height="350"
+            max-width="400"
+            :src="require('@/assets/fipu_hr.png')">
+          </v-img>
+        </v-row>
+      </v-footer>
     </v-row>
   </v-container>
 </template>
@@ -46,15 +84,39 @@ export default {
   data: () => ({
     inquiry: "",
     topics: [],
+    request: false,
   }),
+
   methods: {
     async sendInquiry() {
+      this.request = true;
+      this.topics = [];
       let data = {
         text: this.inquiry,
       };
       let result = await NLP.sendInquiry(data);
-      this.topics = result.data;
-      console.log(result.data);
+
+      let sorted_data = this.sortBySimilarity(result);
+      let final_data = this.setColors(sorted_data);
+
+      this.topics = final_data;
+
+      this.request = false;
+      console.log(this.topics);
+    },
+    sortBySimilarity(unsorted_data) {
+      let sorted_data = unsorted_data.data.sort((a, b) =>
+        a.similarity > b.similarity ? -1 : 1
+      );
+      return sorted_data;
+    },
+    setColors(sorted_data) {
+      sorted_data[0].color = "green";
+      sorted_data[1].color = "orange";
+      sorted_data[2].color = "red";
+      sorted_data[3].color = "red";
+
+      return sorted_data;
     },
   },
 };
